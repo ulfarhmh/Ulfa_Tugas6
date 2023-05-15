8c3bc0477dc27ad5d2767dbf24c6c065faef1101
ulfarhmh17 <ulfa.r20@mhs.unsyiah.ac.id>
2023-05-15 19:22:30 +0700

    Penambahan fungsi perkalian dan pembagian

--- Math_Function.py
+++ Math_Function.py
@@ -1,3 +1,10 @@
-
 def add(a, b):
     return a + b
+
+def multiply(a, b):
+    return a * b
+
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Tidak bisa membagi dengan nol")
+    return a / b
